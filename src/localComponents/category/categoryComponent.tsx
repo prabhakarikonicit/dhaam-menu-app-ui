dca8f1358168ea79d38a3f3f885d78a7abbcecd3
rahroy14 <rahroy14@in.ibm.com>
2025-04-14 17:12:09 +0530

    wip

--- src/localComponents/category/categoryComponent.tsx
+++ src/localComponents/category/categoryComponent.tsx
@@ -20,7 +20,7 @@ const CategoryComponent: React.FC = () => {
   return (
     <Layout viewName="Category" addClass="font-inter">
       <div className="bg-[var(--background-color-backgroundWhite)] flex space-x-2 mb-4 justify-between items-center p-4 rounded-lg">
-        <div className="relative flex-grow">
+        <div className="relative">
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
             {/* <Search className="h-5 w-5 text-gray-400" /> */}
             <SearchIcon />
